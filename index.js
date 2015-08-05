a98bb7bdcd77602b6be1bc7dc1596fb385288a92
Pierre Pointereau <pierreyves.pointereau@news.com.au>
2015-08-05 17:01:12 +1000

    Creating a lib/ file to store the core functions, adapting the index

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ var fs        = require("fs"),
     program   = require("commander"),
     path      = require("path"),
     jade      = require("jade"),
-    manifest  = require("./package.json");
+    manifest  = require("./package.json"),
+    fuzzer    = require("./lib/fuzzer");
 
 // Spit out banner
 console.log(
@@ -42,97 +43,36 @@ console.log("Loading fixture file %s...".cyan.dim, program.fixture);
 var fixture = require(path.join(process.cwd(), program.fixture));
 
 console.log("Loading and compiling jade template %s...".cyan.dim, program.jade);
-var template = jade.compileFile(path.join(process.cwd(), program.jade));
+var template =
+    jade.compileFile(
+        program.jade[0] === "/" ?
+            program.jade :
+            path.join(process.cwd(), program.jade));
 
-function testRender(input) {
-    try {
-        template(input);
-        return true;
-
-    } catch(error) {
-        var message = error.message;
-
-        console.error("\t\tFailed to render template: %s".red, message.split(/\n/)[0]);
-        console.error("\t\t" + message.split(/\n/ig).slice(1).join("\n\t\t").red.dim);
-        return false;
-    }
-}
-
-function isLeafNode(property) {
-    if (!(property instanceof Object))
-        return true;
-
-    if (property instanceof Array && !property.length)
-        return true;
-
-    if (property instanceof Object && !Object.keys(property).length)
-        return true;
-
-    return false;
-}
-
-function iterate(object, replacer) {
-    Object.keys(object).forEach(function(key) {
-        if (isLeafNode(object[key])) {
-            replacer(object, key);
-        } else {
-            iterate(object[key], replacer);
-        }
-    });
-}
-
-var transformations = {
-    "Number Replace": function(object, key) {
-        object[key] = Math.random();
-    },
-    "String Replace": function(object, key) {
-        object[key] = String.fromCharCode(Math.random() * 255 | 0);
-    },
-    "Null Replace": function(object, key) {
-        object[key] = null;
-    },
-    "Empty Object Replace": function(object, key) {
-        object[key] = {};
-    },
-    "Function Replacer": function(object, key) {
-        object[key] = function() {};
-    },
-    "Leaf Node Deletion": function(object, key) {
-        delete object[key];
-    }
-};
 
 // ---------- Let the fuzzing commence!
+var events = fuzzer(fixture, template);
 
-console.log("Verifying basic render...".cyan.dim);
-testRender(fixture);
-
-var iteration = 0,
-    failures  = 0;
-
-while (Object.keys(fixture).length) {
-
-    console.log("Fuzzing iteration %d...".cyan, ++iteration);
-
-    Object.keys(transformations).forEach(function(fuzzerName) {
-        console.log("\tRunning leaf node fuzzer '%s'...".cyan.dim, fuzzerName);
-        iterate(fixture, transformations[fuzzerName]);
-
-        if (!testRender(fixture)) {
-            failures ++;
-        } else {
-            console.log("\t\t-> OK.".dim);
-        }
-    });
+events.emit = function() {
+    console.log(arguments[0]);
 }
 
-console.log("\nConcluded with %d failures.".cyan, failures);
 
-if (failures) {
-    console.error((
-        "The template crashed at least once during fuzzing.\n" +
-        "Consider adding additional type checks where errors occurred."
-    ).red);
-}
+// console.log("\nConcluded with %d failures.".cyan, failures);
 
-process.exit(failures);
+// if (failures) {
+//     console.error((
+//         "The template crashed at least once during fuzzing.\n" +
+//         "Consider adding additional type checks where errors occurred."
+//     ).red);
+// }
+
+// process.exit(failures);
+
+
+
+//
+//
+// //fjsldfm
+// console.error("\t\tFailed to render template: %s".red, message.split(/\n/)[0]);
+// console.error("\t\t" + message.split(/\n/ig).slice(1).join("\n\t\t").red.dim);
